719ff5134387ab91afcf0b1db4414f70488ab259
Noah Agne <noahtagne@gmail.com>
2018-06-29 12:14:28 -0400

    updated methods for reservation DAL, changed CLI order and menu navigation, general cleanup

--- Capstone/CapstoneCLI.cs
+++ Capstone/CapstoneCLI.cs
@@ -34,21 +34,27 @@ namespace Capstone
 					int parkSelection = DisplayAllParks();
 					DisplayParkInfo(parkSelection);
 
-					//string input = Console.ReadLine();
-					//if (input == "1" || input == "2" || input == "3")
-					//{
-					//	DisplayParkInfo(input);
-					//}
 				}
-				//else if (command == Command_Quit)
-				//{
-				//	QuitApplication();
-				//	break;
-				//}
+				else if (mainMenuSelection.ToLower() == "q")
+				{
+					QuitApplication();
+					break;
+				}
 
 				Console.Clear();
+				PrintTitle();
+				Console.WriteLine();
+				PrintHeader();
 				PrintMenu();
 			}
+		}
+
+		private void QuitApplication()
+		{
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.Magenta;
+			Console.WriteLine("Goodbye");
+
 		}
 
 		private void PrintTitle()
@@ -87,6 +93,11 @@ namespace Capstone
 			Console.WriteLine();
 		}
 
+
+		/// <summary>
+		/// Display the list of parks
+		/// </summary>
+		/// <returns>The park id selected by user</returns>
 		private int DisplayAllParks()
 		{
 			ParkSqlDAL dal = new ParkSqlDAL(DatabaseConnection);
@@ -157,6 +168,8 @@ namespace Capstone
 						Console.Clear();
 						campgrounds = DisplayCampgroundInfo(parkId);
 						campgroundId = SelectValidCampground(parkId, campgrounds);
+						break;
+						//SearchForCampgroundReservation(campgroundId, campgrounds);
 						//DisplayAvailableSites(parkId, campgroundId);
 					}
 				}
@@ -164,7 +177,10 @@ namespace Capstone
 				{
 					campgrounds = DisplayCampgroundInfo(parkId);
 					campgroundId = SelectValidCampground(parkId, campgrounds);
-					
+					break;
+					//SearchForCampgroundReservation(campgroundId, campgrounds);
+
+
 					//DisplayAvailableSites(parkId, campgroundId);
 				}
 				else if (parkInfoId == 3)
@@ -172,15 +188,20 @@ namespace Capstone
 					break;
 				}
 
-				if (campgroundId != 0)
-				{
-					SearchForCampgroundReservation();
-				}
+				//if (campgroundId != 0)
+				//{
+				//	SearchForCampgroundReservation(campgroundId, campgrounds);
+				//}
 
 				//DisplayCampgroundInfo(parkId, campgroundId);
 			}
 		}
 
+		/// <summary>
+		/// Displays list of available campgrounds for selected park
+		/// </summary>
+		/// <param name="parkId"></param>
+		/// <returns>The list of campgrounds available</returns>
 		private IList<Campground> DisplayCampgroundInfo(int parkId)
 		{
 			CampgroundSqlDAL dal = new CampgroundSqlDAL(DatabaseConnection);
@@ -205,6 +226,12 @@ namespace Capstone
 			return campgrounds;
 		}
 
+		/// <summary>
+		/// User will choose between available campgrounds at selected park by campground id
+		/// </summary>
+		/// <param name="parkId"></param>
+		/// <param name="campgrounds"></param>
+		/// <returns>campground id selected by user</returns>
 		private int SelectValidCampground(int parkId, IList<Campground> campgrounds)
 		{
 			int campgroundId;
@@ -223,52 +250,151 @@ namespace Capstone
 					}
 				}
 
-				if (campgroundId == 0)
+				if (campgroundId == 0 || !indexExists)
 				{
 					break;
 				}
 				else if (indexExists)
 				{
-					DisplayAvailableSites(parkId, campgroundId);
+					DateTime[] desiredReservationDates = SearchForCampgroundReservation(campgroundId, campgrounds);
+					int siteNumber = DisplayAvailableSites(parkId, campgroundId);
+					Reservation successfulReservation = CheckAvailableReservation(parkId, campgroundId, siteNumber, desiredReservationDates);
+					if (successfulReservation != null)
+					{
+						Console.WriteLine($"The reservation has been made and Confirmation ID is: {successfulReservation.ReservationId}");
+						Console.WriteLine("Thank you for Camping! press any key to return to Main Menu");
+						Console.ReadKey();
+						break;
+					}
 				}
 			}
 			return campgroundId;
 		}
 
-		private DateTime[] SearchForCampgroundReservation()
+		/// <summary>
+		/// Checks the entered requested date range against existing reservations for that selected site at the selected campground at the selected park
+		/// </summary>
+		/// <param name="parkId"></param>
+		/// <param name="campgroundId"></param>
+		/// <param name="siteNumber"></param>
+		/// <param name="desiredReservationDates"></param>
+		/// <returns>Reservation at the selected site at the selected campground at the selected park</returns>
+		private Reservation CheckAvailableReservation(int parkId, int campgroundId, int siteNumber, DateTime[] desiredReservationDates)
+		{
+			ReservationSqlDAL dal = new ReservationSqlDAL(DatabaseConnection);
+			IList<Reservation> reservations = dal.GetReservations(parkId, campgroundId, siteNumber);
+			Reservation successfulReservation = new Reservation();
+			bool isValidReservation = true;
+
+			foreach (var reservation in reservations)
+			{
+				if ((desiredReservationDates[0] >= reservation.StartDate && desiredReservationDates[0] <= reservation.EndDate) || (desiredReservationDates[1] >= reservation.StartDate && desiredReservationDates[1] <= reservation.EndDate))
+				{
+					isValidReservation = false;
+					break;
+
+				}
+			}
+			if (isValidReservation)
+			{
+				Console.Write("Please Enter Reservation Name: ");
+				string reservationName = Console.ReadLine();
+				int siteId = reservations[0].SiteId;
+				successfulReservation = dal.AddReservation(siteId, reservationName, desiredReservationDates);
+
+			}
+			return successfulReservation;
+
+
+
+		}
+
+		/// <summary>
+		/// Collects requested date range for reservation
+		/// </summary>
+		/// <param name="campgroundId"></param>
+		/// <param name="campgrounds"></param>
+		/// <returns>Array of datetime representing requested arrival date and requested departure date</returns>
+		private DateTime[] SearchForCampgroundReservation(int campgroundId, IList<Campground> campgrounds)
 		{
 			DateTime[] desiredReservationDates = new DateTime[2];
+			Campground currentCampground = new Campground();
 
-			Console.WriteLine();
-			Console.WriteLine("What is the arrival date?:");
-			Console.Write("Year (YYYY): ");
-			string arrivalYear = Console.ReadLine();
-			Console.Write("Month (MM): ");
-			string arrivalMonth = Console.ReadLine();
-			Console.Write("Day (DD): ");
-			string arrivalDay = Console.ReadLine();
+			foreach (var campground in campgrounds)
+			{
+				if (campgroundId == campground.CampgroundId)
+				{
+					currentCampground = campground;
+					break;
+				}
+			}
 
-			DateTime arrivalDate = new DateTime(Convert.ToInt32(arrivalYear), Convert.ToInt32(arrivalMonth), Convert.ToInt32(arrivalDay));
-			desiredReservationDates[0] = arrivalDate;
 
-			Console.WriteLine();
+			while (true)
+			{
+				Console.WriteLine();
+				Console.WriteLine("What is the arrival date?:");
+				Console.Write("Year (YYYY): ");
+				string arrivalYear = Console.ReadLine();
+				Console.Write("Month (MM): ");
+				string arrivalMonth = Console.ReadLine();
+				Console.Write("Day (DD): ");
+				string arrivalDay = Console.ReadLine();
 
-			Console.WriteLine("What is the departure date?:");
-			Console.Write("Year (YYYY): ");
-			string departureYear = Console.ReadLine();
-			Console.Write("Month (MM): ");
-			string departureMonth = Console.ReadLine();
-			Console.Write("Day (DD): ");
-			string departureDay = Console.ReadLine();
+				DateTime arrivalDate = new DateTime(Convert.ToInt32(arrivalYear), Convert.ToInt32(arrivalMonth), Convert.ToInt32(arrivalDay));
+				desiredReservationDates[0] = arrivalDate;
 
-			DateTime departureDate = new DateTime(Convert.ToInt32(departureYear), Convert.ToInt32(departureMonth), Convert.ToInt32(departureDay));
-			desiredReservationDates[1] = departureDate;
+				Console.WriteLine();
+
+				Console.WriteLine("What is the departure date?:");
+				Console.Write("Year (YYYY): ");
+				string departureYear = Console.ReadLine();
+				Console.Write("Month (MM): ");
+				string departureMonth = Console.ReadLine();
+				Console.Write("Day (DD): ");
+				string departureDay = Console.ReadLine();
+
+				DateTime departureDate = new DateTime(Convert.ToInt32(departureYear), Convert.ToInt32(departureMonth), Convert.ToInt32(departureDay));
+				desiredReservationDates[1] = departureDate;
+
+				bool validDateRange = CheckDateRange(currentCampground, desiredReservationDates);
+
+				if (validDateRange)
+				{
+					break;
+				}
+				Console.WriteLine("Invalid Date Entered, Please Try Again");
+			}
 
 			return desiredReservationDates;
 		}
 
-		private void DisplayAvailableSites(int parkId, int campgroundId)
+		/// <summary>
+		/// Checks date range from user against open and close date range of campground and its associated sites
+		/// </summary>
+		/// <param name="currentCampground"></param>
+		/// <param name="desiredReservationDates"></param>
+		/// <returns>The open status of the campground for the requested date range</returns>
+		private bool CheckDateRange(Campground currentCampground, DateTime[] desiredReservationDates)
 		{
+			bool validDateRange = false;
+			if (desiredReservationDates[0].Month >= currentCampground.OpenMonth && desiredReservationDates[0].Month <= currentCampground.CloseMonth && desiredReservationDates[1].Month >= currentCampground.OpenMonth && desiredReservationDates[1].Month <= currentCampground.CloseMonth)
+			{
+				validDateRange = true;
+			}
+			return validDateRange;
+		}
+
+		/// <summary>
+		/// Displays the sites available at the selected campground at the selected park
+		/// </summary>
+		/// <param name="parkId"></param>
+		/// <param name="campgroundId"></param>
+		/// <returns>Site number selected by user</returns>
+		private int DisplayAvailableSites(int parkId, int campgroundId)
+		{
+
+			int siteNumber;
 			while (true)
 			{
 				SiteSqlDAL dal = new SiteSqlDAL(DatabaseConnection);
@@ -290,49 +416,24 @@ namespace Capstone
 				Console.WriteLine();
 				Console.Write("Which site should be reserved (enter 0 to cancel)? ");
 
-				Console.ReadLine();
+				siteNumber = Convert.ToInt32(Console.ReadLine());
+				bool validSite = false;
+				foreach (var site in sites)
+				{
+					if (siteNumber == site.SiteNumber)
+					{
+						validSite = true;
+						break;
+					}
+				}
+				if (validSite)
+				{
+					break;
+				}
 				//DisplayAllReservations(sites);
 			}
+			return siteNumber;
 		}
 	}
 }
-
-//		private void DisplayAllReservations(IList<Site> sites)
-//		{
-//			string siteSelection = Console.ReadLine();
-//			while (true)
-//			{
-//				if (siteSelection == "0")
-//				{
-//					break;
-//				}
-//				else
-//				{
-//					Console.Write("What name should the reservation be made under? ");
-//					string reservationName = Console.ReadLine();
-
-//					ReservationSqlDAL dal = new ReservationSqlDAL(DatabaseConnection);
-//					IList<Reservation> reservations = dal.GetReservations(sites[Convert.ToInt32(siteSelection) - 1].SiteId);
-//					if (sites.Count > 0)
-//					{
-//						Console.Clear();
-//						Console.WriteLine("Reservation Id".ToString().PadRight(15) + "Site Id".ToString().PadRight(15) + "Name".ToString().PadRight(15) + "Check In".ToString().PadRight(15) + "Check Out".ToString().PadRight(15) + "Book Date".ToString());
-//						foreach (Reservation reservation in reservations)
-//						{
-//							Console.WriteLine(reservation.ReservationId.ToString().PadRight(15) + reservation.SiteId.ToString().PadRight(15) + reservation.Name.ToString().PadRight(15) + reservation.StartDate.ToString().PadRight(15) + reservation.EndDate.ToString().PadRight(15) + reservation.CreateDate.ToString() + reservation.SiteNumber.ToString());
-//						}
-//					}
-//					else
-//					{
-//						Console.WriteLine("**** SOLD TO PRIVATE CORPORATION-TEDDY ROOSEVELT SPINNING IN GRAVE ****");
-
-//					}
-//					Console.ReadKey();
-
-//				}
-//			}
-//		}
-
-//		
-//		}
 
--- Capstone/DAL/ReservationSqlDAL.cs
+++ Capstone/DAL/ReservationSqlDAL.cs
@@ -17,12 +17,46 @@ namespace Capstone.DAL
 			connectionString = databaseConnectionString;
 		}
 
-		public void AddReservation()
+		public Reservation AddReservation(int siteId,string reservationName,DateTime[] desiredReservationDates)
 		{
+			Reservation reservation = new Reservation();
+			try
+			{
+				using (SqlConnection conn = new SqlConnection(connectionString))
+				{
+					conn.Open();
+
+					SqlCommand cmd = new SqlCommand("INSERT INTO reservation(site_id, name, from_date, to_date,create_date) VALUES(@site_id,@name,@from_date,@to_date,@create_date);", conn);
+					cmd.Parameters.AddWithValue("@site_id", siteId);
+					cmd.Parameters.AddWithValue("@name", reservationName);
+					cmd.Parameters.AddWithValue("@from_date", desiredReservationDates[0]);
+					cmd.Parameters.AddWithValue("@to_date", desiredReservationDates[1]);
+					cmd.Parameters.AddWithValue("@create_date", DateTime.Today);
+
+					cmd.ExecuteNonQuery();
+
+					SqlCommand cmd2 = new SqlCommand("SELECT TOP 1* FROM reservation ORDER BY reservation_id DESC;", conn);
+					SqlDataReader reader = cmd2.ExecuteReader();
+					while (reader.Read())
+					{
+						reservation.ReservationId = Convert.ToInt32(reader["reservation_id"]);
+						reservation.SiteId = Convert.ToInt32(reader["site_id"]);
+						reservation.Name = Convert.ToString(reader["name"]);
+						reservation.StartDate = Convert.ToDateTime(reader["from_date"]);
+						reservation.EndDate = Convert.ToDateTime(reader["to_date"]);
+						reservation.CreateDate = Convert.ToDateTime(reader["create_date"]);
+					}
+				}
+			}
+			catch(SqlException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			return reservation;
 
 		}
 
-		public IList<Reservation> GetReservations(int campgroundId, int parkId, int siteId)
+		public IList<Reservation> GetReservations(int parkId, int campgroundId,  int siteNumber)
 		{
 			List<Reservation> reservationList = new List<Reservation>();
 
@@ -31,10 +65,10 @@ namespace Capstone.DAL
 				using (SqlConnection conn = new SqlConnection(connectionString))
 				{
 					conn.Open();
-					SqlCommand cmd = new SqlCommand("SELECT * FROM reservation JOIN site ON site.site_id = reservation.site_id JOIN campground ON campground.campground_id = site.campground_id JOIN park ON park.park_id = campground.park_id WHERE park.park_id = @park_id AND campground.campground.id = @campground.id AND site.site_id = @site_id;", conn);
+					SqlCommand cmd = new SqlCommand("SELECT * FROM reservation JOIN site ON site.site_id = reservation.site_id JOIN campground ON campground.campground_id = site.campground_id JOIN park ON park.park_id = campground.park_id WHERE park.park_id = @park_id AND campground.campground_id = @campground_id AND site.site_number = @site_number;", conn);
 					cmd.Parameters.AddWithValue("@campground_id", campgroundId);
 					cmd.Parameters.AddWithValue("@park_id", parkId);
-					cmd.Parameters.AddWithValue("@site_id", siteId);
+					cmd.Parameters.AddWithValue("@site_number", siteNumber);
 
 					SqlDataReader reader = cmd.ExecuteReader();
 					while (reader.Read())
@@ -48,9 +82,22 @@ namespace Capstone.DAL
 						reservation.EndDate = Convert.ToDateTime(reader["to_date"]);
 						reservation.CreateDate = Convert.ToDateTime(reader["create_date"]);
 						reservation.SiteNumber = Convert.ToInt32(reader["site_number"]);
+						reservation.MaxOccupancy = Convert.ToInt32(reader["max_occupancy"]);
+						reservation.IsAccessible = Convert.ToBoolean(reader["accessible"]);
+						reservation.MaxRVLength = Convert.ToInt32(reader["max_rv_length"]);
+						reservation.HasUtilities = Convert.ToBoolean(reader["utilities"]);
+						reservation.CampgroundId = Convert.ToInt32(reader["campground_id"]);
+						reservation.DailyFee = Convert.ToDecimal(reader["daily_fee"]);
+						reservation.CampgroundName = Convert.ToString(reader["name"]);
+						reservation.CampgroundOpenMonth = Convert.ToInt32(reader["open_from_mm"]);
+						reservation.CampgroundCloseMonth = Convert.ToInt32(reader["open_to_mm"]);
+						reservation.ParkId = Convert.ToInt32(reader["park_id"]);
+						reservation.ParkName = Convert.ToString(reader["name"]);
+						reservation.ParkLocation = Convert.ToString(reader["location"]);
+
 
 						reservationList.Add(reservation);
-					}
+											}
 				}
 			}
 			catch (SqlException ex)
@@ -59,6 +106,7 @@ namespace Capstone.DAL
 			}
 			return reservationList;
 		}
+		
 
 	}
 }
